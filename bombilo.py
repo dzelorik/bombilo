03d6c54aa56b101de4c9c6a64de1a2bbb1b53697
dzelorik <73631044+dzelorik@users.noreply.github.com>
2021-01-01 23:02:02 +0300

    Add files via upload

---
+++ bombilo.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import re
+import sys
+from b0mb3r.cli import main
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
+    sys.exit(main())
